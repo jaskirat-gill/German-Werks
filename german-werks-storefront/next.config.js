eb67b8012885503662312a3dfd0a43e0108fbd8f
Jaskirat Gill <gilljaskirat04@gmail.com>
2024-09-28 23:54:17 -0700

    update storefront image hosting

--- german-werks-storefront/next.config.js
+++ german-werks-storefront/next.config.js
@@ -17,15 +17,7 @@ const nextConfig = withStoreConfig({
       },
       {
         protocol: "https",
-        hostname: "medusa-public-images.s3.eu-west-1.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.amazonaws.com",
-      },
-      {
-        protocol: "https",
-        hostname: "medusa-server-testing.s3.us-east-1.amazonaws.com",
+        hostname:"german-werks.s3.us-east-2.amazonaws.com", // CHANGE IF NEEDED
       },
     ],
   },
